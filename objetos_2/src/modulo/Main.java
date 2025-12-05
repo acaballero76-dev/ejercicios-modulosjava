f48adc3806fcc1a557b6ed81207bf40c021e39f2
acaballero76-dev <aristides.caballero@cuautla.tecnm.mx>
2025-12-05 09:31:04 -0600

    9. Módulo: Sistema de Reservas Aéreas
    Diseña una clase que represente un Vuelo. 
     Atributos: El código del vuelo, la capacidad máxima de asientos y el número actual de 
    asientos reservados. 
     Comportamiento 1: Un método que intente realizar una Reserva de un asiento, 
    incrementando el número de asientos reservados solo si aún hay capacidad disponible. 
     Comportamiento 2: Un método que calcule y retorne el Porcentaje de Ocupación del 
    vuelo.

---
+++ objetos_2/src/modulo/Main.java
@@ -0,0 +1,8 @@
+package modulo;
+
+public class Main {
+    public static void main(String[] args) {
+        Estudiante alumno=new Estudiante("Aristides",95.4,80.4);
+        System.out.println(alumno.nombre);
+    }
+}
